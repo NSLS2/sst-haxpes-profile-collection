91a0afd9d997e092fe7bce2243f30b4345587282
maffettone <phillip.maffettone@gmail.com>
2022-04-22 14:12:03 -0400

    remove bad import. Nonexistent in package.

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -13,6 +13,6 @@ for path in paths:
 
 import nslsii
 import haxpes
-from haxpes.startup import *
+# from haxpes.startup import *
 
 nslsii.configure_base(get_ipython().user_ns, "haxpes", publish_documents_with_kafka=True)
