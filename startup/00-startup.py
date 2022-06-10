46bfd97365ac6d681d1749ce5b5c4d9adab199c9
Conan Weiland <cweiland@bnl.gov>
2022-06-10 11:59:06 -0400

    fixed collection_package paths

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 paths = [
     path
     for path in Path(
-        "/nsls2/data/sst1/ucal/shared/config/bluesky/collection_packages"
+        "/nsls2/data/sst/haxpes/shared/config/bluesky/collection_packages"
     ).glob("*")
     if path.is_dir()
 ]
@@ -13,6 +13,6 @@ for path in paths:
 
 import nslsii
 import haxpes
-# from haxpes.startup import *
+from haxpes.startup import *
 
 nslsii.configure_base(get_ipython().user_ns, "haxpes", publish_documents_with_kafka=True)
