ee4b5cadca2e80415edbf1c242ea9993f0135f26
SST Operator <xf07id1@xf07id-ws2.nsls2.bnl.local>
2026-02-02 17:00:43 -0500

    Working at start of cycle

--- scripts/qs-dev.sh
+++ scripts/qs-dev.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 set -e
 set -o xtrace
+
+pip install -e /home/xf07id1/collection_packages/nbs-core
 pip install -e /home/xf07id1/collection_packages/nbs-bl
 pip install -e /home/xf07id1/collection_packages/sst_base
-pip install -e /home/xf07id1/collection_packages/haxpes
+
 $(dirname "$0")/qs-start.sh
