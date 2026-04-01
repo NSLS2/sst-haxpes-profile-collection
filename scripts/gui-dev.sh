491152c814a3afc02567321756877ba9611094e0
SST Operator <xf07id1@xf07id-ws2.nsls2.bnl.local>
2026-04-01 10:58:17 -0400

    [CW] changes to deal with mode switching

--- scripts/gui-dev.sh
+++ scripts/gui-dev.sh
@@ -2,6 +2,7 @@
 set -e
 set -o xtrace
 
+pip install -e /home/xf07id1/collection_packages/nbs-core
 pip install -e /home/xf07id1/collection_packages/nbs-bl
 pip install -e /home/xf07id1/collection_packages/nbs-gui
 pip install -e /home/xf07id1/collection_packages/sst_base
