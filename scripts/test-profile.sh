29044a6daa741358fd018f4365966dca74702746
Charles Titus <ctitus@bnl.gov>
2026-04-16 18:42:44 -0400

    Trying to figure out tests

--- scripts/test-profile.sh
+++ scripts/test-profile.sh
@@ -8,4 +8,4 @@ TESTS_DIR="${PROFILE_ROOT}/tests"
 
 export TESTS_DIR
 
-ipython --profile collection -c 'import os, sys, pytest, time; time.sleep(10);rc = pytest.main(["-v", os.environ["TESTS_DIR"]]); sys.stdout.flush(); sys.stderr.flush(); os._exit(rc)'
+ipython --profile collection -c 'import os, sys, pytest, time; time.sleep(10); print(os.environ["TESTS_DIR"]); rc = pytest.main(["-v", os.environ["TESTS_DIR"]]); sys.stdout.flush(); sys.stderr.flush(); os._exit(rc)'
