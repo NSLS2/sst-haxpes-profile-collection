2d78c563e196995fdb9a586be255a092b350d668
Charles Titus <ctitus@bnl.gov>
2026-04-20 15:29:32 -0400

    Removing --no-deps flag after wheel surgery...

--- scripts/qs-start.sh
+++ scripts/qs-start.sh
@@ -4,7 +4,7 @@ set -o xtrace
 
 pip install -e /home/xf07id1/collection_packages/haxpes
 if [ -f /nsls2/data/sst/legacy/HAXPES/PeakSDK/peak_sdk-1.5.27-py3-none-any.whl ]; then
-    pip install --no-deps /nsls2/data/sst/legacy/HAXPES/PeakSDK/peak_sdk-1.5.27-py3-none-any.whl
+    pip install /nsls2/data/sst/legacy/HAXPES/PeakSDK/peak_sdk-1.5.27-py3-none-any.whl
 fi
 
 start-re-manager --redis-addr ${QS_REDIS_ADDR:-localhost}:6379 --zmq-publish-console ON --use-ipython-kernel ON --ipython-kernel-ip auto --startup-profile collection
